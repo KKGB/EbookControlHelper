27c4465e27d3fd62c2f07338bd2cc4e059793e86
KKGB <kyungbeom8@naver.com>
2025-05-25 20:08:10 +0900

    Refactoring

--- inference.sh
+++
@@ -1,4 +0,0 @@
-yolo task=segment mode=predict \
-     model=runs/segment/train/weights/best.pt \
-     source=$(find data/images/val -name '*.jpg' | shuf -n 20) \
-     save=True save_masks=True
--- val.sh
+++
@@ -1,5 +0,0 @@
-yolo task=segment mode=val \
-     model=runs/segment/train/weights/best.pt \
-     data=eye_seg_dataset.yaml \
-     batch=16 imgsz=640 device=0 \
-     save_txt=True save_json=True
