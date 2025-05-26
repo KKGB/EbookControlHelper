0dddb774c8ca80eaad9356efef539628cb3d13f0
KKGB <kyungbeom8@naver.com>
2025-05-27 02:12:05 +0900

    Add: keyboard control (mac)

--- src/control.py
+++ src/control.py
@@ -1,7 +1,16 @@
-import keyboard
-import yaml
 import os
+import platform
+import yaml
 from utils.path import resource_path
+
+IS_MAC = platform.system() == "Darwin"
+IS_WIN = platform.system() == "Windows"
+
+if IS_WIN:
+    import keyboard as win_keyboard
+elif IS_MAC:
+    import pyautogui
+
 
 def _load_gaze_actions(config_path="keymap/config.yaml"):
     config_path = resource_path(config_path)
@@ -14,14 +23,25 @@ def _load_gaze_actions(config_path="keymap/config.yaml"):
 _gaze_actions = _load_gaze_actions()
 _last_command = None
 
+def send_key(key: str, repeat: int):
+    for _ in range(repeat):
+        if IS_WIN:
+            win_keyboard.send(key)
+        elif IS_MAC:
+            if "+" in key:
+                keys = key.split("+")
+                pyautogui.hotkey(*keys)
+            else:
+                pyautogui.press(key)
+
 def control_pdf(gaze_state: int, process_name: str):
     global _last_command
 
-    if process_name.lower() != "msedge.exe":
+    target_process = process_name.lower()
+    if (IS_WIN and target_process != "msedge.exe") or (IS_MAC and target_process != "microsoft edge"):
         return
 
     action = _gaze_actions.get(gaze_state)
-
     if action is None:
         _last_command = None
         return
@@ -30,10 +50,8 @@ def control_pdf(gaze_state: int, process_name: str):
         return
 
     _last_command = gaze_state
-
     key, repeat = action
-    for _ in range(repeat):
-        keyboard.send(key)
+    send_key(key, repeat)
 
 def control_youtube(gaze_state: int, process_name: str):
     pass
